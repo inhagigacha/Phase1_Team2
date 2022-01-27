875ba1f6a7001b0509257777d071ffa75d548698
miryeong <bark9757@naver.com>
2022-01-28 03:57:18 +0900

    Auto stash before merge of "development" and "origin/development"

--- src/new_gigacha/scripts/map/sd_mapping/gps_to_sd.py
+++ src/new_gigacha/scripts/map/sd_mapping/gps_to_sd.py
@@ -9,13 +9,18 @@ def gps_to_sd():
       left_lane_y_reader = csv.reader(left_lane_file[1])
       right_lane_x_reader = csv.reader(right_lane_file[0])
       right_lane_y_reader = csv.reader(right_lane_file[1])
+      llx = list(left_lane_x_reader)
+      lly = list(left_lane_y_reader)
+      rlx = list(right_lane_x_reader)
+      rly = list(right_lane_y_reader)
 
   with open ('x.csv', mode="w", newline='') as x_writer:
     with open ('y.csv', mode="w", newline='') as y_writer:
-      for s in range(left_lane_x_reader):
-        x_writer.writerow([left_lane_x_reader[s], right_lane_x_reader[s]])
-        y_writer.writerow([left_lane_y_reader[s], right_lane_y_reader[s]])
+      for s in range(llx):
+        x_writer.writerow([llx[s], rlx[s]])
+        y_writer.writerow([lly[s], rly[s]])
 
+<<<<<<< Updated upstream:src/new_gigacha/scripts/map/sd_mapping/gps_to_sd.py
 if __name__=="main":
   gps_to_sd()
 =======
@@ -24,3 +29,6 @@ d1 = pd.read_csv('C:/Users/ss/Desktop/maps/lane1.csv', header=None,delimiter=" "
 d2 = pd.read_csv('C:/Users/ss/Desktop/map2/x.csv', header=None,delimiter=" ")
 d2.iloc[:, [0]]=d1[0]
 >>>>>>> Stashed changes
+=======
+gps_to_sd()
+>>>>>>> Stashed changes:src/new_gigacha/scripts/lib/sd_mapping/gps_to_sd.py
