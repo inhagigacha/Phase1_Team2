2571739886b19cd03fd0db883137866bda997b3a
miryeong <bark9757@naver.com>
2022-01-27 23:05:29 +0900

    gps_to_sd first commit

---
+++ src/new_gigacha/scripts/lib/sd_mapping/gps_to_sd.py
@@ -0,0 +1,17 @@
+import numpy as np
+import csv
+
+def gps_to_sd():
+
+  with open ('lane1.csv', mode="r") as left_lane_file:
+    with open ('lane2.csv', mode="r") as right_lane_file:
+      left_lane_x_reader = csv.reader(left_lane_file[0])
+      left_lane_y_reader = csv.reader(left_lane_file[1])
+      right_lane_x_reader = csv.reader(right_lane_file[0])
+      right_lane_y_reader = csv.reader(right_lane_file[1])
+
+  with open ('x.csv', mode="w", newline='') as x_writer:
+    with open ('y.csv', mode="w", newline='') as y_writer:
+      for s in range(left_lane_x_reader):
+        x_writer.writerow([left_lane_x_reader[s], right_lane_x_reader[s]])
+        y_writer.writerow([left_lane_y_reader[s], right_lane_y_reader[s]])
