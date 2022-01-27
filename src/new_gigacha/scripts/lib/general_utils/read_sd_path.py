571a95364c83d3012c3dee3396bd19ea7daac9c9
hyunswim <coldsz@naver.com>
2022-01-28 01:20:09 +0900

    resolve read_sd_path code issue

--- src/new_gigacha/scripts/lib/general_utils/read_sd_path.py
+++ src/new_gigacha/scripts/lib/general_utils/read_sd_path.py
@@ -9,12 +9,12 @@ def read_sd_map():
         with open("/home/cvlab/gigacha/planning_ws/src/new_gigacha/scripts/map/sd_simul/y.csv", mode="r") as y_csv_file:
             x_csv_reader = csv.reader(x_csv_file)
             y_csv_reader = csv.reader(y_csv_file)
-            # print("1")
-            print(f"x_csv_reader : {x_csv_reader}")
-            # return
-            for s in x_csv_reader:
-                for d in range(len(x_csv_reader[0])):
-                    sd_map[s][d] = (x_csv_reader[s][d], y_csv_reader[s][d])
+            x = list(x_csv_reader)
+            y = list(y_csv_reader)
+            for s in range(len(x)):
+                for d in range(len(x[1])):
+                    sd_map.append([float(x[s][d]), float(y[s][d])])
+                    print("check")
 
     return sd_map
 
