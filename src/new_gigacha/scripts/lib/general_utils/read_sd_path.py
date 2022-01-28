be2fd68bc6354f9116ec17007d689443c568d3dc
cheon12 <12170432@inha.edu>
2022-01-28 17:12:17 +0900

    path change
    absolute path 2 relative path

--- src/new_gigacha/scripts/lib/general_utils/read_sd_path.py
+++ src/new_gigacha/scripts/lib/general_utils/read_sd_path.py
@@ -6,8 +6,8 @@ def read_sd_map():
     
     sd_map_x = []
     sd_map_y = []
-    with open("/home/cvlab/gigacha/planning_ws/src/new_gigacha/scripts/map/sd_simul/x.csv", mode="r") as x_csv_file:
-        with open("/home/cvlab/gigacha/planning_ws/src/new_gigacha/scripts/map/sd_simul/y.csv", mode="r") as y_csv_file:
+    with open("../../map/sd_simul/x.csv", mode="r") as x_csv_file:
+        with open("../../map/sd_simul/y.csv", mode="r") as y_csv_file:
             x_csv_reader = csv.reader(x_csv_file)
             y_csv_reader = csv.reader(y_csv_file)
             x = list(x_csv_reader)
