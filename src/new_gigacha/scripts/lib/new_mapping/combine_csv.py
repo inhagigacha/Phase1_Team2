c37a60ba66c92299140fb76d310f49976b80695b
miryeong <bark9757@naver.com>
2022-02-16 03:18:02 +0900

    combine_csv 3rd_modified

--- src/new_gigacha/scripts/lib/new_mapping/combine_csv.py
+++ src/new_gigacha/scripts/lib/new_mapping/combine_csv.py
@@ -3,15 +3,17 @@ import csv
 def combine_csv(number_of_files):
     
     real_array = []
+    float_array = []
 
     for i in range(1, number_of_files):
-        with open(f'/maps/{i}_map.csv', mode="r", newline = '') as csv_file:
+        with open(f'/home/gigacha/Phase1_Team2/src/new_gigacha/scripts/lib/new_mapping/maps/{i}_map.csv', mode="r", newline = '') as csv_file:
             for line in csv_file.readlines():
                 array = line.split(',')
-                real_array.append(array)
+                float_array = [float(array[0]), float(array[1])]
+                real_array.append(float_array)
             
-    with open('/maps/all_nodes.csv', 'wb', newline = '') as all_nodes_file: 
+    with open('/home/gigacha/Phase1_Team2/src/new_gigacha/scripts/lib/new_mapping/maps/all_nodes.csv', 'w', newline = '') as all_nodes_file: 
         writer = csv.writer(all_nodes_file)
         writer.writerows(real_array)
 
-combine_csv(9)
+combine_csv(10)
