99f45e53b0b0a963f5330b32ae7a83a0a881aeb9
miryeong <bark9757@naver.com>
2022-02-16 04:26:45 +0900

    combine_csv complete & added maps

--- src/new_gigacha/scripts/lib/new_mapping/combine_csv.py
+++ src/new_gigacha/scripts/lib/new_mapping/combine_csv.py
@@ -6,13 +6,13 @@ def combine_csv(number_of_files):
     float_array = []
 
     for i in range(1, number_of_files):
-        with open(f'/home/gigacha/Phase1_Team2/src/new_gigacha/scripts/lib/new_mapping/maps/{i}_map.csv', mode="r", newline = '') as csv_file:
+        with open(f'/home/gigacha/Phase1_Team2/src/new_gigacha/src/new_gigacha/scripts/lib/new_mapping/maps/{i}_map.csv', mode="r", newline = '') as csv_file:
             for line in csv_file.readlines():
                 array = line.split(',')
                 float_array = [float(array[0]), float(array[1])]
                 real_array.append(float_array)
             
-    with open('/home/gigacha/Phase1_Team2/src/new_gigacha/scripts/lib/new_mapping/maps/all_nodes.csv', 'w', newline = '') as all_nodes_file: 
+    with open('/home/gigacha/Phase1_Team2/src/new_gigacha/src/new_gigacha/scripts/lib/new_mapping/maps/all_nodes.csv', 'w', newline = '') as all_nodes_file: 
         writer = csv.writer(all_nodes_file)
         writer.writerows(real_array)
 
